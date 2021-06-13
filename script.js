f7c3d427dbff9919498d85599882dbf18379b8bc
Ajeeth Natarajan <43535972+iamajeeth@users.noreply.github.com>
2021-06-13 12:32:38 +0530

    Adding Base files index, style and script

---
+++ script.js
@@ -0,0 +1,13 @@
+function generate(){
+    var length = document.getElementById("los").value;
+    var result = random(length);
+    document.getElementById("result").value = result;
+}
+function random(length){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
